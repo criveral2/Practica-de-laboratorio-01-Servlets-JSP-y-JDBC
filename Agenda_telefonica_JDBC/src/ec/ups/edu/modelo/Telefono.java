cad8bbcbb482b1b314c34087249fee8c61f9aa50
Christian <criveral2@est.ups.edu.ec>
2020-05-05 16:42:00 -0500

    Creacion de login y registros

---
+++ Agenda_telefonica_JDBC/src/ec/ups/edu/modelo/Telefono.java
@@ -0,0 +1,39 @@
+package ec.ups.edu.modelo;
+
+import java.io.Serializable;
+
+public class Telefono implements Serializable{
+	private static final long serialVersionUID = 1L;
+	private int codigo;
+	private String numero;
+	private String tipo;
+	private String operadora;
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getOperadora() {
+		return operadora;
+	}
+	public void setOperadora(String operadora) {
+		this.operadora = operadora;
+	}
+	
+	
+	
+
+}
---
+++ Agenda_telefonica_JDBC/src/ec/ups/edu/modelo/Usuario.java
@@ -0,0 +1,60 @@
+package ec.ups.edu.modelo;
+
+import java.io.Serializable;
+
+public class Usuario  implements Serializable{
+	private static final long serialVersionUID = 1L;
+	private  String cedula;
+	private String nombre;
+	private String apellido;
+	private String correo;
+	private String contrasena;
+	
+	public Usuario(String ced, String nom, String apell, String corr, String contra) {
+		this.cedula = ced;
+		this.nombre = nom;
+		this.apellido = apell;
+		this.correo = corr;
+		this.contrasena = contra;
+	}
+	
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	public String getContrasena() {
+		return contrasena;
+	}
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [cedula=" + cedula + ", nombre=" + nombre + ", apellido=" + apellido + ", correo=" + correo
+				+ ", contrasena=" + contrasena + "]";
+	}
+	
+	
+
+}
